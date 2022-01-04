32a85bd17110296b8b092d52e4846e190f807790
eluinstra <eluinstra@luin.dev>
2022-01-04 22:28:31 +0100

    created scala 3 project

---
+++ src/main/scala/dev/luin/file/server/fileserverscala/Main.scala
@@ -0,0 +1,8 @@
+package dev.luin.file.server
+
+@main def Main(args: String*): Unit =
+  println("─" * 100)
+
+  println("hello world")
+
+  println("─" * 100)
---
+++ src/test/scala/dev/luin/file/server/fileserverscala/ExampleSuite.scala
@@ -0,0 +1,6 @@
+package dev.luin.file.server
+
+final class ExampleSuite extends TestSuite:
+  test("hello world") {
+    1 `shouldBe` 1
+  }
